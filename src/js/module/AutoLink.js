b5e0feead47d661a502e53143a12efcf63dad2e8
Pawel Czekaj <pawel.czekaj@ziflow.com>
2023-11-16 10:25:22 +0100

    feat: paste clipboard image behind the config flag

--- src/js/module/AutoLink.js
+++ src/js/module/AutoLink.js
@@ -20,13 +20,8 @@ export default class AutoLink {
       'summernote.keydown': (we, event) => {
         this.handleKeydown(event);
       },
-      'summernote.paste': () => {
-        setTimeout(() => {
-          const wordRange = this.context.invoke('editor.createRange').getWordRange(false);
-          this.lastWordRange = wordRange;
-
-          this.replace();
-        }, 10);
+      'summernote.paste': (we, event) => {
+        this.onPaste(event.originalEvent);
       },
     };
   }
@@ -37,6 +32,56 @@ export default class AutoLink {
 
   destroy() {
     this.lastWordRange = null;
+  }
+
+  onPaste(e) {
+    const clipboardData = e.clipboardData || window.clipboardData;
+    const pastedData = clipboardData.getData('Text');
+
+    // Regex to find URL in the text
+    const urlRegex = /https?:\/\/\S+\b/gi;
+    let match = urlRegex.exec(pastedData);
+
+    if (match) {
+      e.preventDefault();
+
+      let lastIndex = 0;
+      let fragment = document.createDocumentFragment();
+      // Process all matches and surrounding text
+      do {
+        // Append text before URL
+        fragment.appendChild(
+          document.createTextNode(pastedData.substring(lastIndex, match.index))
+        );
+
+        // Create and append the anchor element for the URL
+        const url = match[0];
+        const anchor = $('<a>').attr('href', url).text(url);
+        fragment.appendChild(anchor[0]);
+
+        lastIndex = urlRegex.lastIndex;
+      } while ((match = urlRegex.exec(pastedData)) !== null);
+
+      // Append any remaining text after the last URL
+      fragment.appendChild(
+        document.createTextNode(pastedData.substring(lastIndex))
+      );
+
+      const marker = document.createElement('span');
+      fragment.appendChild(marker);
+
+      // Insert the processed content
+      const range = this.context.invoke('editor.createRange');
+      range.insertNode(fragment);
+
+      const selectionRange = document.createRange();
+      selectionRange.setStartAfter(marker);
+      selectionRange.collapse(true); // true to collapse the range to its start point
+
+      const selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(selectionRange);
+    }
   }
 
   replace() {
@@ -61,7 +106,7 @@ export default class AutoLink {
       this.lastWordRange.insertNode(node);
       this.lastWordRange = null;
 
-      range.createFromNodeAfter(node).select();
+      this.context.invoke('editor.focus');
       this.context.triggerEvent('change', this.$editable.html(), this.$editable);
     }
   }
